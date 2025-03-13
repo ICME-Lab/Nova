65b9a5db66d5c8e81af91e0586c2427121282e75
Forpee <khalilhassam3@gmail.com>
2025-03-13 22:03:07 +0200

    chore: get timings

--- src/r1cs/mod.rs
+++ src/r1cs/mod.rs
@@ -19,7 +19,7 @@ use once_cell::sync::OnceCell;
 use rand_core::OsRng;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::marker::PhantomData;
+use std::{marker::PhantomData, time::Instant};
 
 mod sparse;
 pub(crate) use sparse::SparseMatrix;
@@ -431,7 +431,9 @@ impl<E: Engine> R1CSShape<E> {
       .map(|(((az, bz), cz), e)| *az * *bz - u * *cz - *e)
       .collect::<Vec<E::Scalar>>();
 
+    let time = Instant::now();
     let comm_T = CE::<E>::commit(ck, &T, r_T);
+    println!("Commitment time: {:?}", time.elapsed());
 
     Ok((T, comm_T))
   }
